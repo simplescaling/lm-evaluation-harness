83931e25af9c38bd25360ad17a48432f2715df9a
Ubuntu <zc096373@gmail.com>
2025-05-03 00:33:17 +0000

    adding humanity's last exam

--- lm_eval/tasks/hle/utils.py
+++ lm_eval/tasks/hle/utils.py
@@ -1,69 +1,40 @@
 from collections import Counter
 import logging
 import os
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Literal
 import string
+import asyncio
+import json
+from pydantic import BaseModel
 
 from simpleverify import verify_generic
 from datasets import Dataset
 from simpleverify.verify_generic import clean, ChatCompletionSampler
+from openai import AsyncOpenAI
 
+client = AsyncOpenAI(timeout=300.0, max_retries=1)
 eval_logger = logging.getLogger(__name__)
 
-QUERY_TEMPLATE = '{Question}'
+JUDGE_PROMPT = """Judge whether the following [response] to [question] is correct or not based on the precise and unambiguous [correct_answer] below.
 
-EXTRACTION_TEMPLATE = r"""
-Look at the following question and an attempt by a student and extract which choice among capital letter A-Z the student picked. If the student did not pick any choice, respond with "-1".
+[question]: {question}
 
-Examples:
+[response]: {response}
 
-    Question: ...
-    Attempt: Answer: **A**
+Your judgement must be in the format and criteria specified below:
 
-A
+extracted_final_answer: The final exact answer extracted from the [response]. Put the extracted answer as 'None' if there is no exact, final answer to extract from the response.
 
-    Question: A) Dinosaur B) Elephant C) Cat D) Dog
-    Attempt: ...The answer is therefore Elephant...
+[correct_answer]: {correct_answer}
 
-B
+reasoning: Explain why the extracted_final_answer is correct or incorrect based on [correct_answer], focusing only on if there are meaningful differences between [correct_answer] and the extracted_final_answer. Do not comment on any background to the problem, do not attempt to solve the problem, do not argue for any answer different than [correct_answer], focus only on whether the answers match.
 
-    Question: ...
-    Attempt: Answer: None of the above
+correct: Answer 'yes' if extracted_final_answer matches the [correct_answer] given above, or is within a small margin of error for numerical problems. Answer 'no' otherwise, i.e. if there is any inconsistency, ambiguity, non-equivalency, or if the extracted answer is incorrect.
 
--1
-
-    Question: ...
-    Attempt: ...Answer: D), because...
-
-D
-
-    Question: ...
-(A) 7 
-(B) 8 
-(C) 4 
-(D) 10
-    Attempt: 4
-
-C
-
-    Question: ...
-    Attempt: ...\\boxed{C}...
-
-C
-
----
-
-YOUR TASK
-
-
-Respond only with the capitalized alphabetic letter (without quotes) or -1. Do not include a rationale.
-
-    Question: %(expression1)s
-    Attempt: %(expression2)s
-""".strip()
+confidence: The extracted confidence score between 0|\%| and 100|\%| from [response]. Put 100 if there is no confidence score available."""
 
 def doc_to_text(doc: dict) -> str:
-    return QUERY_TEMPLATE.format(Question=doc.get("problem", doc.get("question")))
+    return doc.get("question")
 
 def process_docs(dataset: Dataset) -> Dataset:
     def _process_doc(doc: dict) -> dict:
@@ -76,10 +47,34 @@ def process_docs(dataset: Dataset) -> Dataset:
         return out_doc
     return dataset.map(_process_doc)
 
-def extract_answer(sampler, question: str, attempt: str):
-   prompt = EXTRACTION_TEMPLATE % {"expression1": question, "expression2": attempt}
-   response = sampler([dict(content=prompt, role="user")])
-   return response
+class ExtractedAnswer(BaseModel):
+    extracted_final_answer: str
+    reasoning: str
+    correct: Literal["yes", "no"]
+    confidence: int
+    strict: Literal[True] # 100% reliability
+
+async def extract_answer(question: str, correct_answer: str, response: str, model: str ="gpt-4.1-mini-2025-04-14") -> Dict:
+    prompt = JUDGE_PROMPT.format(question=question, correct_answer=correct_answer, response=response)
+    try:
+        completion = await client.beta.chat.completions.parse(
+            model=model,
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0,
+            response_format=ExtractedAnswer,
+            max_completion_tokens=4096,
+        )
+        content = completion.choices[0].message.parsed
+        return {
+            "correct_answer": correct_answer,
+            "model_answer": content.extracted_final_answer,
+            "reasoning": content.reasoning,
+            "correct": content.correct,
+            "confidence": content.confidence
+        }
+    except Exception as e:
+        eval_logger.warning(f"Judging failed: {e}")
+        return None
 
 def process_results(
     doc: dict,
@@ -87,7 +82,7 @@ def process_results(
     tokenizer=None,
     max_len=32768,
 ) -> Dict[str, int]:
-    metrics = {"exact_match": None, "extracted_answers": []}
+    metrics = {"exact_match": None, "extracted_answers": [], "confidence": None}
     # Multiple results -> we are measuring cov/maj etc
     if isinstance(results[0], list):
         results = results[0]
@@ -112,83 +107,46 @@ def process_results(
             **{f"tok_ans@{n}": -1 for n in n_stats_list},
             **{f"too_long@{n}": -1 for n in n_stats_list},
         }
+        
+        
+    SEP = os.getenv("SEP", "</think>")
+    
+    for i, a in enumerate(results, start=1):
+        if tokenizer is not None:
+            parts = a.split(SEP, 1)
+            metrics["tok_think"].append(len(tokenizer.tokenize(parts[0])))
+            metrics["tok_ans"].append(0 if len(parts) == 1 else len(tokenizer.tokenize(parts[1])))
+            metrics["tok"].append(len(tokenizer.tokenize(a)))
+            metrics["too_long"].append(metrics["tok"][-1] >= max_len)
+            if i in n_stats_list:
+                metrics[f"tok@{i}"] = sum(metrics["tok"]) / len(metrics["tok"])
+                metrics[f"tok_think@{i}"] = sum(metrics["tok_think"]) / len(metrics["tok_think"])
+                metrics[f"tok_ans@{i}"] = sum(metrics["tok_ans"]) / len(metrics["tok_ans"])
+                metrics[f"too_long@{i}"] = sum(metrics["too_long"]) / len(metrics["too_long"])
 
-    if doc["answer_type"] == "exactMatch":
-        if isinstance(doc["answer"], str) and doc["answer"].isdigit():
-            gt = str(int(doc["answer"])) # 023 -> 23
+        cleaned = clean(a, sep=SEP)
+        answer_data = asyncio.run(extract_answer(doc["problem"], doc["answer"], cleaned, model="gpt-4.1-mini-2025-04-14"))
+        if answer_data is None:
+            pred_ans = "-1"
+            correct = False
+            confidence = 0
         else:
-            gt = str(doc["answer"])
+            pred_ans = answer_data["model_answer"]
+            correct = answer_data["correct"] == "yes"
+            confidence = answer_data["confidence"]
 
-        SEP = os.getenv("SEP", "</think>")
-        for i, a in enumerate(results, start=1):
-            if tokenizer is not None:
-                parts = a.split(SEP, 1)
-                metrics["tok_think"].append(len(tokenizer.tokenize(parts[0])))
-                metrics["tok_ans"].append(0 if len(parts) == 1 else len(tokenizer.tokenize(parts[1])))
-                metrics["tok"].append(len(tokenizer.tokenize(a)))
-                metrics["too_long"].append(metrics["tok"][-1] >= max_len)
-                if i in n_stats_list:
-                    metrics[f"tok@{i}"] = sum(metrics["tok"]) / len(metrics["tok"])
-                    metrics[f"tok_think@{i}"] = sum(metrics["tok_think"]) / len(metrics["tok_think"])
-                    metrics[f"tok_ans@{i}"] = sum(metrics["tok_ans"]) / len(metrics["tok_ans"])
-                    metrics[f"too_long@{i}"] = sum(metrics["too_long"]) / len(metrics["too_long"])
+        metrics["extracted_answers"].append(pred_ans)
+        metrics["exact_match"] = correct
+        metrics["confidence"] = confidence
 
-            match, x, y = verify_generic(a, gt, sep=SEP)[0]
-            metrics["extracted_answers"].append(gt if match else x)
-            if not(match): # Optional logging
-                print("Marked incorrect\na " + metrics["extracted_answers"][-1] + "\ndoc['answer'] " + gt)
-            if i == 1:
-                metrics["exact_match"] = match
-                if "exact_matches" in metrics:
-                    metrics["exact_matches"].append(match)
-            elif i > 1:
-                metrics["exact_matches"].append(match)
-                if i in n_res_list:
-                    metrics[f"cov@{i}"] = int(1 in metrics["exact_matches"])
-                    metrics[f"maj@{i}"] = int(gt == Counter(metrics["extracted_answers"]).most_common(1)[0][0])
-                    metrics[f"avg@{i}"] = sum(metrics["exact_matches"]) / i
-    elif doc["answer_type"] == "multipleChoice":
-        sampler = ChatCompletionSampler(model="gpt-4o-mini")
-        question = doc["question"]
-        SEP = os.getenv("SEP", "</think>")
-        for i, a in enumerate(results, start=1):
-            if tokenizer is not None:
-                parts = a.split(SEP, 1)
-                metrics["tok_think"].append(len(tokenizer.tokenize(parts[0])))
-                metrics["tok_ans"].append(0 if len(parts) == 1 else len(tokenizer.tokenize(parts[1])))
-                metrics["tok"].append(len(tokenizer.tokenize(a)))
-                metrics["too_long"].append(metrics["tok"][-1] >= max_len)
-                if i in n_stats_list:
-                    metrics[f"tok@{i}"] = sum(metrics["tok"]) / len(metrics["tok"])
-                    metrics[f"tok_think@{i}"] = sum(metrics["tok_think"]) / len(metrics["tok_think"])
-                    metrics[f"tok_ans@{i}"] = sum(metrics["tok_ans"]) / len(metrics["tok_ans"])
-                    metrics[f"too_long@{i}"] = sum(metrics["too_long"]) / len(metrics["too_long"])
-
-            a = clean(a, sep=SEP)
-
-            if a in list(string.ascii_lowercase):
-                a = a.upper()
-            elif a not in list(string.ascii_uppercase):
-                a = extract_answer(sampler, question, a)
-                if a not in list(string.ascii_uppercase):
-                    print(f"Warning: Default to A as given {results[i-1]} extracted {a}")
-                    a = "A"
-
-            metrics["extracted_answers"].append(a)
-            match = a == doc["answer"]
-            a = int(a == doc["answer"])
-            if not(a): # Optional logging
-                print("Marked incorrect\na " + metrics["extracted_answers"][-1] + "\ndoc['answer'] " + doc["answer"])
-            if i == 1:
-                metrics["exact_match"] = match
-                if "exact_matches" in metrics:
-                    metrics["exact_matches"].append(match)
-            elif i > 1:
-                metrics["exact_matches"].append(match)
-                if i in n_res_list:
-                    metrics[f"cov@{i}"] = int(1 in metrics["exact_matches"])
-                    metrics[f"maj@{i}"] = int(doc["answer"] == Counter(metrics["extracted_answers"]).most_common(1)[0][0])
-                    metrics[f"avg@{i}"] = sum(metrics["exact_matches"]) / i
+        if i == 1:
+            metrics["exact_match"] = correct
+        elif i > 1:
+            metrics["exact_matches"].append(correct)
+            if i in n_res_list:
+                metrics[f"cov@{i}"] = int(1 in metrics["exact_matches"])
+                metrics[f"maj@{i}"] = int(doc["answer"] == Counter(metrics["extracted_answers"]).most_common(1)[0][0])
+                metrics[f"avg@{i}"] = sum(metrics["exact_matches"]) / i
     
     
     return metrics
